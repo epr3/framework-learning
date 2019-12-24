29aca980fe0d16394399bea7a98331bd34c5c0db
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-12-24 21:23:17 +0200

    added angular material

--- angular-django/frontend/src/app/app.module.ts
+++ angular-django/frontend/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from "./app.component";
 import { AuthModule } from "./auth/auth.module";
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { GuestLayoutComponent } from './guest-layout/guest-layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, AuthLayoutComponent, GuestLayoutComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })

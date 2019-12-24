d4d576b49b096ef76bc651a62e4f2357c2c73cd2
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-12-24 21:21:33 +0200

    WIP frontend scaffolding

--- angular-django/frontend/src/app/app.module.ts
+++ angular-django/frontend/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AuthModule } from "./auth/auth.module";
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { GuestLayoutComponent } from './guest-layout/guest-layout.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AuthLayoutComponent, GuestLayoutComponent],
   imports: [BrowserModule, AppRoutingModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent]

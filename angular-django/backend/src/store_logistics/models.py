377b77efe490ab08c61bdc9331a5fad3bd707e7a
Eduard Predescu <eduard.florin.predescu@gmail.com>
2020-01-19 19:06:07 +0200

    done models for store_logistics

--- angular-django/backend/src/store_logistics/models.py
+++ angular-django/backend/src/store_logistics/models.py
@@ -1,3 +1,37 @@
+from uuid import uuid4
+from functools import reduce
 from django.db import models
+from core.models import Profile
+from books.models import Book
 
-# Create your models here.
+
+class Address(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    county = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=10)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    status = models.IntegerField(choices=list(zip(
+        ['placed', 'in processing', 'prepared for delivery', 'in delivery', 'delivered'], range(0, 6))))
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    delivery_address = models.ForeignKey(
+        Address, on_delete=models.DO_NOTHING, related_name='order_delivery_address')
+    billing_address = models.ForeignKey(
+        Address, on_delete=models.DO_NOTHING, related_name='order_billing_address')
+    books = models.ManyToManyField(Book, through='OrderBooks')
+
+    @property
+    def order_value(self):
+        return reduce(lambda x, y: x.book.price * x.quantity + y.book.price * y.quantity, self.books)
+
+
+class OrderBooks(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()

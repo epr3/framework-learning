50f22ac2cac6e23c4e806a56bcce5f93172454fd
Eduard Predescu <eduard.florin.predescu@gmail.com>
2020-01-25 16:08:05 +0200

    Done profile service tests

--- angular-django/frontend/src/app/profile/profile.service.spec.ts
+++ angular-django/frontend/src/app/profile/profile.service.spec.ts
@@ -1,18 +1,87 @@
 import { TestBed } from "@angular/core/testing";
 
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from "@angular/common/http/testing";
 
 import { ProfileService } from "./profile.service";
 
 describe("ProfileService", () => {
-  beforeEach(() =>
+  const fakeProfile = {
+    name: "",
+    surname: "",
+    telephone: "",
+    user: {
+      email: "test@test.com"
+    }
+  };
+
+  let service: ProfileService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
-    })
-  );
+      imports: [HttpClientTestingModule],
+      providers: [ProfileService]
+    });
+    service = TestBed.get(ProfileService);
+    service.log = jest.fn();
+    service.handleError = jest.fn();
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it("should be created", () => {
-    const service: ProfileService = TestBed.get(ProfileService);
     expect(service).toBeTruthy();
+  });
+
+  it("should get the profile and return it", () => {
+    service.getProfile().subscribe(res => {
+      expect(res).toBe(fakeProfile);
+      expect(service.log).toHaveBeenCalled();
+    });
+
+    const req = httpMock.expectOne("http://localhost:8000/profile/");
+    expect(req.request.method).toBe("GET");
+    req.flush(fakeProfile);
+  });
+
+  it("should log error when you get the profile", () => {
+    service.getProfile().subscribe(_ => {
+      expect(service.handleError).toHaveBeenCalledWith("fetch profile");
+      expect(service.log).toHaveBeenCalled();
+    });
+
+    const req = httpMock.expectOne("http://localhost:8000/profile/");
+
+    req.error(new ErrorEvent("Forbidden"), {
+      status: 400,
+      statusText: "error"
+    });
+  });
+
+  it("should update the profile and return it", () => {
+    service.putProfile(fakeProfile).subscribe(res => {
+      expect(res).toBe(fakeProfile);
+      expect(service.log).toHaveBeenCalled();
+    });
+
+    const req = httpMock.expectOne("http://localhost:8000/profile/");
+    expect(req.request.method).toBe("PUT");
+    req.flush(fakeProfile);
+  });
+
+  it("should log error when you update", () => {
+    service.putProfile(fakeProfile).subscribe(_ => {
+      expect(service.handleError).toHaveBeenCalledWith("update profile");
+      expect(service.log).toHaveBeenCalled();
+    });
+
+    const req = httpMock.expectOne("http://localhost:8000/profile/");
+
+    req.error(new ErrorEvent("Forbidden"), {
+      status: 400,
+      statusText: "error"
+    });
   });
 });
